384e786b6b57a55e9fa4e0b880203b6b6427b7d2
Remi Paulmier <remi.paulmier@gmail.com>
2022-02-18 00:07:02 +0100

    fix column name according to json

--- bam/models.py
+++ bam/models.py
@@ -10,16 +10,18 @@ association_table = db.Table(
 
 
 class Episode(db.Model):
+    __tablename__ = "episode"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     air_date = db.Column(db.Date, nullable=False)
     episode = db.Column(db.String, nullable=False)
     characters = db.relationship(
-        "Character", secondary=association_table, back_populates="episodes"
+        "Character", secondary=association_table, back_populates="episode"
     )
 
 
 class Character(db.Model):
+    __tablename__ = "character"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     status = db.Column(db.String, nullable=False)
