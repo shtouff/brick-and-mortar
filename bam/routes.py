f2b8ca40c0f982ebd90f2ae17a0c2c4a6f1a4a21
Remi Paulmier <remi.paulmier@gmail.com>
2022-02-18 00:26:47 +0100

    feature 1 OK

--- bam/routes.py
+++ bam/routes.py
@@ -1,7 +1,37 @@
 from flask import Flask, jsonify
 
+from bam import models, types
+
 
 def register_routes(app: Flask):
-    @app.route("/foo")
-    def get_foo():
-        return jsonify({})
+    @app.route("/api/character")
+    def get_characters():
+        return jsonify(
+            [
+                types.Character(
+                    id=c.id,
+                    name=c.name,
+                    status=c.status,
+                    species=c.species,
+                    type=c.type,
+                    gender=c.gender,
+                    episode=([e.id for e in c.episode]),
+                )
+                for c in models.Character.query.all()
+            ]
+        )
+
+    @app.route("/api/episode")
+    def get_episodes():
+        return jsonify(
+            [
+                types.Episode(
+                    id=e.id,
+                    name=e.name,
+                    episode=e.episode,
+                    air_date=e.air_date,
+                    characters=([c.id for c in e.characters]),
+                )
+                for e in models.Episode.query.all()
+            ]
+        )
